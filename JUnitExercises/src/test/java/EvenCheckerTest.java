9c6b7bd8f92e6287fadbc5c037ebf96dfe27075b
sadhana208 <146124680+sadhana208@users.noreply.github.com>
2026-07-02 20:48:20 +0530

    Advanced JUnit Exercise 1 - Parameterized Tests

---
+++ JUnitExercises/src/test/java/EvenCheckerTest.java
@@ -0,0 +1,21 @@
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class EvenCheckerTest {
+
+    EvenChecker checker = new EvenChecker();
+
+    @ParameterizedTest
+    @ValueSource(ints = {2, 4, 6, 8, 10})
+    void testEvenNumbers(int number) {
+        assertTrue(checker.isEven(number));
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 3, 5, 7, 9})
+    void testOddNumbers(int number) {
+        assertFalse(checker.isEven(number));
+    }
+}
