d8a5ea6b1d53b22b6ff3fef5ddb87647296a82ab
sadhana208 <146124680+sadhana208@users.noreply.github.com>
2026-07-10 14:56:58 +0530

    ReactJS Handson 1

--- 09-Spring-Data-JPA/orm-learn/src/main/java/com/cognizant/orm_learn/OrmLearnApplication.java
+++ 09-Spring-Data-JPA/orm-learn/src/main/java/com/cognizant/orm_learn/OrmLearnApplication.java
@@ -6,7 +6,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContext;4
 
 import java.util.List;
 
