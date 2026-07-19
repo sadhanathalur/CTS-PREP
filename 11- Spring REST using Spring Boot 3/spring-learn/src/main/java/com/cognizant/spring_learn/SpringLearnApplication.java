ff01603952dd39057fe55c04a75340db71e24bf4
sadhana208 <146124680+sadhana208@users.noreply.github.com>
2026-07-19 22:20:47 +0530

    Hello World RESTful Web Service handson

--- 11- Spring REST using Spring Boot 3/spring-learn/src/main/java/com/cognizant/spring_learn/SpringLearnApplication.java
+++ 11- Spring REST using Spring Boot 3/spring-learn/src/main/java/com/cognizant/spring_learn/SpringLearnApplication.java
@@ -1,5 +1,5 @@
 package com.cognizant.spring_learn;
-
+import org.springframework.boot.SpringApplication;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -13,7 +13,7 @@ public class SpringLearnApplication {
 			LoggerFactory.getLogger(SpringLearnApplication.class);
 
 	public static void main(String[] args) {
-		displayCountry();
+		SpringApplication.run(SpringLearnApplication.class, args);
 	}
 
 	public static void displayCountry() {
