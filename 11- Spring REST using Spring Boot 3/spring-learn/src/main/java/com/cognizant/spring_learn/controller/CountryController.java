a96af801537802c18ed731d8ecc64f79fe3a59a0
sadhana208 <146124680+sadhana208@users.noreply.github.com>
2026-07-20 16:14:49 +0530

    REST Get Country Code handson

--- 11- Spring REST using Spring Boot 3/spring-learn/src/main/java/com/cognizant/spring_learn/controller/CountryController.java
+++ 11- Spring REST using Spring Boot 3/spring-learn/src/main/java/com/cognizant/spring_learn/controller/CountryController.java
@@ -1,11 +1,12 @@
 package com.cognizant.spring_learn.controller;
 
 import com.cognizant.spring_learn.Country;
+import com.cognizant.spring_learn.service.CountryService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -14,15 +15,15 @@ public class CountryController {
     private static final Logger LOGGER =
             LoggerFactory.getLogger(CountryController.class);
 
-    @RequestMapping("/country")
-    public Country getCountryIndia() {
+    @Autowired
+    private CountryService countryService;
+
+    @GetMapping("/countries/{code}")
+    public Country getCountry(@PathVariable String code) {
 
         LOGGER.info("START");
 
-        ApplicationContext context =
-                new ClassPathXmlApplicationContext("country.xml");
-
-        Country country = context.getBean("country", Country.class);
+        Country country = countryService.getCountry(code);
 
         LOGGER.info("END");
 
